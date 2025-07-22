fea1b695f7ede91f7a372ffbf2405c5889a39241
Luis Alvarez <johnlavz34@gmail.com>
2025-07-21 19:15:47 -0500

    setting DOM variables

---
+++ toDoList.js
@@ -0,0 +1,6 @@
+// DOOM variables
+
+const task = document.getEmentById("task");
+const add = document.getElementById("add");
+const filter = document.getElementById("filter");
+const taskList = document.getElementById("task-list");
