88ac78a5db786de9fc692c4dd16df6d77f054667
Luis Alvarez <johnlavz34@gmail.com>
2025-07-22 22:01:14 -0500

    global variable fltr added

--- toDoList.js
+++ toDoList.js
@@ -6,3 +6,5 @@ const filter = document.getElementById("filter");
 const taskList = document.getElementById("task-list");
 
 const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+let fltr = "all";
