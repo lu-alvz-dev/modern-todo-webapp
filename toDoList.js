3e8d7f4022fd9b1a690764925e3b5debdf1d875d
Luis Alvarez <johnlavz34@gmail.com>
2025-07-29 16:49:30 -0500

    fixxing synax to get section HTML element from index HTML

--- toDoList.js
+++ toDoList.js
@@ -2,7 +2,7 @@
 
 const task = document.getEmentById("task");
 const add = document.getElementById("add");
-const filter = document.getElementById("filter");
+const filter = document.querySelector(".filter");
 const taskList = document.getElementById("task-list");
 
 const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
